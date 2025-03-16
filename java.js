6b1b1c3ac19329daff9c628c3611f7edd3d9b708
Rustam-7 <joraqulovamahfuza7@gmail.com>
2025-03-16 22:54:26 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,12 @@
+
+let birinchi = Math.floor(Math.random() * 100) + 1;
+let ikkinchi = Math.floor(Math.random() * 100) + 1;
+
+
+let uchinchi = birinchi * ikkinchi ;
+
+
+let tortinchi = birinchi / 2;
+let bishinchi = ikkinchi / 2;
+
+alert(`Число 1: ${birinchi}\nЧисло 2: ${ikkinchi}\n\nПроизведение: ${tortinchi}\n${birinchi} / 2 = ${uchinchi}\n${ikkinchi} / 2 = ${tortinchi}`);
